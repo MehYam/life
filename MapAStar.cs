MehYam/life

commit 9aad02773455fd1402e41a6faf4cd80125bb640d

replace width/height with size

MapAStar.cs
@@ -27,7 +27,7 @@ namespace lifeEngine
                     if (!(x == 0 && y == 0))
                     {
                         Point<int> newPos = new Point<int>(node.position.x + x, node.position.y + y);
-                        if (newPos.x >= 0 && newPos.x < _layer.width && newPos.y >= 0 && newPos.y < _layer.height)
+                        if (newPos.x >= 0 && newPos.x < _layer.size.x && newPos.y >= 0 && newPos.y < _layer.size.y)
                         {
                             if (_isTilePassable(_layer.Get(newPos.x, newPos.y)))
                             {
@@ -53,8 +53,8 @@ namespace lifeEngine
             return false;
         }
 
-        int ToIndex(Point<int> position) { return position.y * _layer.width + position.x; }
-        Point<int> ToPosition(int index) { return new Point<int>(index % _layer.width, index / _layer.width); }
+        int ToIndex(Point<int> position) { return position.y * _layer.size.x + position.x; }
+        Point<int> ToPosition(int index) { return new Point<int>(index % _layer.size.x, index / _layer.size.y); }
 
         static int GetDistance(Point<int> src, Point<int> dest)
         {

Map.cs
@@ -9,20 +9,16 @@ namespace lifeEngine
     {
         readonly T[,] tiles;
 
-        public readonly int width;
-        public readonly int height;
-
+        public readonly Point<int> size;
         public Layer(int width, int height)
         {
             tiles = new T[width, height];
-            this.width = width;
-            this.height = height;
+            this.size = new Point<int>(width, height);
         }
         public Layer(Layer<T> rhs)
         {
-            tiles = new T[rhs.width, rhs.height];
-            this.width = rhs.width;
-            this.height = rhs.height;
+            tiles = new T[rhs.size.x, rhs.size.y];
+            this.size = new Point<int>(rhs.size.x, rhs.size.y);
 
             ForEach((x, y, tile) =>
             {
@@ -52,9 +48,9 @@ namespace lifeEngine
         /// <param name="callback"></param>
         public void ForEach(Action<int, int, T> callback)
         {
-            for (var y = 0; y < height; ++y)
+            for (var y = 0; y < size.y; ++y)
             {
-                for (var x = 0; x < width; ++x)
+                for (var x = 0; x < size.x; ++x)
                 {
                     callback(x, y, tiles[x, y]);
                 }
@@ -62,9 +58,9 @@ namespace lifeEngine
         }
         public void Fill(Func<int, int, T, T> callback)
         {
-            for (var y = 0; y < height; ++y)
+            for (var y = 0; y < size.y; ++y)
             {
-                for (var x = 0; x < width; ++x)
+                for (var x = 0; x < size.x; ++x)
                 {
                     tiles[x, y] = callback(x, y, tiles[x, y]);
                 }

Operations.cs
@@ -19,7 +19,7 @@ namespace lifeEngine
                 for (int y = 0; y < lines.Length; ++y)
                 {
                     var line = lines[y];
-                    var extent = Math.Min(line.Length, retval.width);
+                    var extent = Math.Min(line.Length, retval.size.x);
                     for (int x = 0; x < line.Length; ++x)
                     {
                         retval.Set(new Point<int>(x, y), new Tile(line[x]));

Util.cs
@@ -85,5 +85,10 @@ namespace lifeEngine
         {
             return NearlyEqual(a.x, b.x) && NearlyEqual(a.y, b.y);
         }
+        public static bool Within(Point<int> testPoint, Point<int> min, Point<int> max)
+        {
+            return min.x <= testPoint.x && testPoint.x <= max.x && min.y <= testPoint.y && testPoint.y <= max.y;
+        }
+        public static readonly Point<int> zero = new Point<int>(0, 0);
     }
 }

World.cs
@@ -35,6 +35,11 @@ namespace lifeEngine
             map = new Layer<Tile>(width, height);
             items = new Layer<Tile>(width, height);
         }
+        public World(Layer<Tile> mapLayer)
+        {
+            map = mapLayer;
+            items = new Layer<Tile>(mapLayer.size.x, mapLayer.size.y);
+        }
         const long DATETIME_TICKS_PER_SEC = 10 * 1000 * 1000;
         long _lastTick;
         public void Tick()
@@ -56,6 +61,13 @@ namespace lifeEngine
                 }
             }
             _lastTick = now;
+        }
+        public void Tick(float time, float deltaTime)
+        {
+            foreach (var actor in _actors)
+            {
+                actor.FixedUpdate(time, deltaTime);
+            }
         }
         List<Actor> _actors = new List<Actor>();
         public void AddActor(Actor a)

lifeTest/Program.cs
@@ -177,14 +177,14 @@ namespace lifeTest
             {
                 return new TestTile('.');
             });
-            for (int c = 0; c < layer.width; c += 2)
+            for (int c = 0; c < layer.size.x; c += 2)
             {
-                for (int r = 1; r < layer.height; ++r)
+                for (int r = 1; r < layer.size.y; ++r)
                 {
                     layer.Set(c, r, new TestTile('x'));
                 }
                 c += 2;
-                for (int r = 0; r < layer.height - 1; ++r)
+                for (int r = 0; r < layer.size.y - 1; ++r)
                 {
                     layer.Set(c, r, new TestTile('x'));
                 }
@@ -253,7 +253,7 @@ namespace lifeTest
 
                 bool spanLeft = false;
                 bool spanRight = false;
-                while (y1 < layer.height && fillCondition(new Point<int>(temp.x, y1)))
+                while (y1 < layer.size.y && fillCondition(new Point<int>(temp.x, y1)))
                 {
                     fillAction(new Point<int>(temp.x, y1));
                     ++pointsColored;
@@ -267,12 +267,12 @@ namespace lifeTest
                     {
                         spanLeft = false;
                     }
-                    if (!spanRight && temp.x < layer.width - 1 && fillCondition(new Point<int>(temp.x + 1, y1)))
+                    if (!spanRight && temp.x < layer.size.x - 1 && fillCondition(new Point<int>(temp.x + 1, y1)))
                     {
                         points.Push(new Point<int>(temp.x + 1, y1));
                         spanRight = true;
                     }
-                    else if (spanRight && temp.x < layer.width - 1 && fillCondition(new Point<int>(temp.x + 1, y1)))
+                    else if (spanRight && temp.x < layer.size.x - 1 && fillCondition(new Point<int>(temp.x + 1, y1)))
                     {
                         spanRight = false;
                     }
@@ -337,7 +337,7 @@ namespace lifeTest
         }
         static Layer<string> RenderHeat(Layer<float> temps)
         {
-            var retval = new Layer<string>(temps.width, temps.height);
+            var retval = new Layer<string>(temps.size.x, temps.size.y);
             temps.ForEach((x, y, temp) =>
             {
                 retval.Set(x, y, string.Format("{0} ", (int)Math.Round(temp)));
@@ -350,7 +350,7 @@ namespace lifeTest
 
             Console.WriteLine(layer);
 
-            var temps = new Layer<float>(layer.width, layer.height);
+            var temps = new Layer<float>(layer.size.x, layer.size.y);
 
             // first, set the outside temperatures to 0 and indoors to 9
             const float AMBIENT = 0;
@@ -367,8 +367,8 @@ namespace lifeTest
                 {
                     if (x > 0) ExchangeHeat(layer, temps, x, y, x - 1, y);
                     if (y > 0) ExchangeHeat(layer, temps, x, y, x, y - 1);
-                    if (x < layer.width - 1) ExchangeHeat(layer, temps, x, y, x + 1, y);
-                    if (y < layer.height - 1) ExchangeHeat(layer, temps, x, y, x, y + 1);
+                    if (x < layer.size.x - 1) ExchangeHeat(layer, temps, x, y, x + 1, y);
+                    if (y < layer.size.y - 1) ExchangeHeat(layer, temps, x, y, x, y + 1);
                 });
 
                 // next, "accelerate" all outdoor temps to the ambient temperature

commit 6914a16b5305e394c7d609d73810e123a7dfb408

fix bug introduced in 9aad02773455fd1402e41a6faf4cd80125bb640d

MapAStar.cs
@@ -54,7 +54,7 @@ namespace lifeEngine
         }
 
         int ToIndex(Point<int> position) { return position.y * _layer.size.x + position.x; }
-        Point<int> ToPosition(int index) { return new Point<int>(index % _layer.size.x, index / _layer.size.y); }
+        Point<int> ToPosition(int index) { return new Point<int>(index % _layer.size.x, index / _layer.size.x); }
 
         static int GetDistance(Point<int> src, Point<int> dest)
         {